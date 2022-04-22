953ec8032379d41769c4899ebe63258d46d2341b
aragosta1226 <djaragosta1226@gmail.com>
2022-04-22 09:57:17 +0900

    first commit

---
+++ fukuokapzm.php
@@ -0,0 +1,60 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="「福岡のパーティーを絶対に盛り上げる.com」
+  福岡でパーティーを行う際に弊社をご利用いただければ必ず盛り上げます。
+  結婚式の２次会、懇親会、文化祭、企業のパーティーなど、どこでも出張可能です。
+  会場準備や音響機材・スピーカーのご用意、イベント企画までなんでもご相談ください。">
+  <meta name="keywords" content="fukuoka,出張dj,dj派遣,福岡,fukuokaparty,
+  wedding,weddinparty,結婚式２次会,企業パーティー,ビーチパーティー,文化祭,モーターショー,
+  レセプションパーティー,ホームパーティー,クローズパーティー,キャンプパーティー,partyplanner,
+  party,sounddesign,eventplanner,福岡イベント,福岡パーティー,音響レンタル,懇親会,幹事代行,
+  beachparty,homeparty,fashionshow,fukuokacity,福岡のパーティー絶対盛り上げるドットコム">
+  <link rel="stylesheet" href="./css/style_pzm.css">
+  <title>福岡のパーティー絶対盛り上げる.com</title>
+</head>
+<body>
+  <!---------------------------------------------------------------------------
+    * メインビジュアル
+  ------------------------------------------------------------------------------>
+  <div class="mainvisual">
+    <img src="./img/main.png" width="100%" height="100%" alt="福岡のパーティー絶対盛り上げるDJ機材">
+    <h1 class="main_title">DoKoDeMo DJ</h1>
+    <div class="text1">福岡のパーティー絶対盛り上げる.com</div>
+  </div> 
+  <!-----------------------------------------------------------------------------
+    * NEWS
+  ------------------------------------------------------------------------------>
+  <section class="about" id="about"> 
+      <div class="inner">
+          <h3>簡単にDJを呼べます</h3>
+          <div class="about-box">
+              <div class="about-card-wrapper">
+                  <div class="about-card">
+                      <img src="./img/dj.png" alt="DJ">
+                      <p class="about1">どんなパーティーに<br>DJを呼べるの？</p>
+                      <p class="about-coment">スペースさえあればどこでもDJがあなたのパーティーに出張い<br>たします。現役のDJがパーティーの規模にあったプランをご<br>提案します。どんなパーティーにDJを呼べるのかな？等、お<br>気軽にご相談ください。ホームパーティーももちろん可！</p>
+                  </div>
+                  <div class="about-card">
+                      <img src="./img/suguyoyaku.png" alt="予約">
+                      <p class="about1">スグに予約OK</p>
+                      <p class="about-coment">急にパーティーすることになった、来週DJ頼んでみようか<br>な？等でも、DJのスケジュールが空いていれば、いつでも対応<br>可能です。まずはご相談ください。</p>
+                  </div>
+                  <div class="news-card">
+                      <img src="./img/itsudemo.png" alt="確認">
+                      <p class="about1">準備の状況確認が<br>いつでも出来る！</p>
+                      <p class="about-coment">これまでいちいち電話やメールで把握しないといけなかった<br>準備の状況がいつでも簡単に確認できます。譲許確認が出来る事で<br>今まで面倒だった幹事の手間を減らします。</p>
+                  </div>
+              </div>
+              <a href="#" class="news-btn">お問い合わせはこちら</a>
+          </div>
+      </div>
+</section>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style_login.css">
+    <title>ログイン画面</title>
+</head>
+<body>
+    <form name="login_form">
+        <div class="login_form_top">
+            <img src="./img/pzmlogo.png" alt="ロゴ">
+        </div>
+        <div class="login_form_btn">
+            <input type="id" name="user_id" placeholder="UserID" required="required"><br>
+            <input type="password" name="password" placeholder="Password" required="required">
+            <input type="submit" name="botton" value="LOGIN">
+        </div>
+    </form>
+</body>
+</html>

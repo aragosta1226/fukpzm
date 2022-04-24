18222573dfe8053cbaff5ee573e3df4c2a3f6616
aragosta1226 <djaragosta1226@gmail.com>
2022-04-24 19:50:02 +0900

    コードの追加

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
     <h1>福岡のパーティー絶対盛り上げる.com</h1>
     <nav>
       <ul id="g-navi">
-        <li><a href="#">お問い合わせ</a></li>
+        <li><a href="#form">お問い合わせ</a></li>
       </ul>
     </nav>
   </header>
@@ -74,13 +74,13 @@
     </li>
   </ul>
     <div>
-    <a href="#" class="">お問い合わせはこちら</a>
+    <a href="#form">お問い合わせはこちら</a>
     </div>
   <h2><strong>サービスについて</strong></h2>
+  <h2><strong>お客様のパーティーのニーズにあったDJを手配します。</strong></h2>
   <div class="flex-item">
     <div class="flex-item_img"><img src="./img/dj2.png" alt=""></div>
     <div class="flex-item_txt">
-      <h2><strong>お客様のパーティーのニーズにあったDJを手配します。</strong></h2>
       <p><strong>現役の実績のあるプロのDJが対応します！</strong></p>
       <p>どんなシーンでも対応力のあるDJをご用意いたします。在籍しているDJは、さまざ<br>
       まなシーンでプレイをしてきた実績のある現役のプロのDJです。もちろん事前に当<br>
@@ -93,68 +93,68 @@
     <div>
       <p>point 1</p>
       <h2>パーティーに詳しい現役DJが担当</h2>
-      <p>さまざまなシーンで実績を積んできたプロのDJがパーティーでDJするので安心して
-        開催する事ができます。どんなパーティーにしたいかをしっかりとお打ち合わせ頂きまして、
+      <p>さまざまなシーンで実績を積んできたプロのDJがパーティーでDJするので安心して<br>
+        開催する事ができます。どんなパーティーにしたいかをしっかりとお打ち合わせ頂きまして、<br>
         お客様の為に熟練のDJがイチから選曲リストを作成し、当日皆様がハッピーになっていただける様にします。</p>
     </div>
     <div>
       <p>point 2</p>
       <h2>今まで目に見えなかった進捗状況の確認ができる</h2>
-      <p>今まで準備の進み具合が確認できなくて不安だったと思います。弊社はそんな進捗状況をちゃんと確認
-        する事が出来ます。パーティー当日までの、「今どんな状況になっているのかな？」という不安を解消します。
+      <p>今まで準備の進み具合が確認できなくて不安だったと思います。弊社はそんな進捗状況をちゃんと確認<br>
+        する事が出来ます。パーティー当日までの、「今どんな状況になっているのかな？」という不安を解消します。<br>
         主催される方の気持ちに寄り添います。</p>
     </div>
   </div>
-  <a href="#" class="news-btn">お問い合わせはこちら</a>
+  <a href="#form">お問い合わせはこちら</a>
   <h2><strong>ご相談の流れ</strong></h2>
   <div>
     <div>
       <p>Step 1</p>
       <h2>お問い合わせ</h2>
-      <p>実際にどんな事が出来るのか知りたいと思います。
-        不安な事など、一緒に解決していければと考えています。
+      <p>実際にどんな事が出来るのか知りたいと思います。<br>
+        不安な事など、一緒に解決していければと考えています。<br>
         まずはお気軽にお問い合わせください。</p>
     </div>
     <div>
       <p>Step 2</p>
       <h2>ヒアリング</h2>
-      <p>お問い合わせ頂きましたら、弊社よりメールにて
-        日程を決めさせて頂き、担当者のみカメラオンにてZOOM等で
-        お打ち合わせさせて頂きます。相談料などは頂きませんので、
+      <p>お問い合わせ頂きましたら、弊社よりメールにて<br>
+        日程を決めさせて頂き、担当者のみカメラオンにてZOOM等で<br>
+        お打ち合わせさせて頂きます。相談料などは頂きませんので、<br>
         お気軽にお話ししましょう！。</p>
     </div>
     <div>
       <p>Step 3</p>
       <h2>準備の進捗状況の確認</h2>
-      <p>パーティーの内容や金額に関して、気に入ってい頂きましたら、
-        再度内容の確認等を行っていただければさっそく準備に入ります。
+      <p>パーティーの内容や金額に関して、気に入ってい頂きましたら、<br>
+        再度内容の確認等を行っていただければさっそく準備に入ります。<br>
         準備の進捗状況に関して、丁寧に対応させて頂きます。</p>
     </div>
     <div>
       <p>Step 4</p>
       <h2>イベント当日</h2>
-      <p>イベント当日は、現場の確認等させて頂きますのでパーティー開始の
+      <p>イベント当日は、現場の確認等させて頂きますのでパーティー開始の<br>
         1時間程度前から現場入りさせて頂きますのでご安心ください。</p>
     </div>
   </div>
-  <a href="#" class="news-btn">お問い合わせはこちら</a>
+  <a href="#form">お問い合わせはこちら</a>
   <h2><strong>よくあるご質問</strong></h2>
   <div>
     <div>
       <h2><strong>イベントの知識が全くないのですが、大丈夫ですか？</strong></h2>
-      <p>イベント開催について丁寧にご説明いたしますので、ご安心ください。
+      <p>イベント開催について丁寧にご説明いたしますので、ご安心ください。<br>
         オプションで会場準備からお手伝いすることも可能です。不安なことは一緒に解決して行きましょう！！</p>
     </div>
     <div>
       <h2><strong>電源がないところで開催したいのですが、可能ですか？</strong></h2>
-      <p>もちろん可能です！金額については、まずはお見積もりからさせて頂きますのでご納得頂いた上での
-        準備になります。弊社は野外イベントのノウハウなどもございますのでご安心ください。</p>
+      <p>もちろん可能です！金額については、まずはお見積もりからさせて頂きますのでご納得頂いた上で<br>
+        準備に入ります。弊社は野外イベントのノウハウなどもございますのでご安心ください。</p>
     </div>
   </div>
   <!---------------------------------------------------------------------------
       * ユーザー登録フォーム
   ------------------------------------------------------------------------------>
-  <h2><a href="#"><strong>まずはお気軽にお問い合わせください！</strong></a></h2>
+  <a name="form" id="form"><strong>まずはお気軽にお問い合わせください！</strong></a>
   <form action="" method="POST">
     <p>
       <label for="">セイ：</label>
@@ -186,18 +186,24 @@
     </p>
   </form>
   <!---------------------------------------------------------------------------
-      * ユーザー登録フォーム
+      * SNS
   ------------------------------------------------------------------------------>
   <h2><strong>SNSでも相談受付してます！！</strong></h2>
-  <!-- LINE -->
-  <p><strong>LINE</strong></p>
-  <p>LINEでも相談受付中です！！</p>
-  <div class="line-it-button" data-lang="ja" data-type="friend" data-env="REAL"   data-lineId="@835gurzh" style="display: none;"></div>
-  <script src="https://www.line-website.com/social-plugins/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
-  <!-- Twitter -->
-  <p><strong>Twitter</strong></p>
-  <p>TwitterのDMでも相談受付中！！</p>
-  <a href="https://twitter.com/fukuokapzm?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @fukuokapzm</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+  <ul class="sns">
+    <!-- LINE -->
+    <li>
+      <p><strong>LINE</strong></p>
+      <p>LINEでも相談受付中です！！</p>
+      <div class="line-it-button" data-lang="ja" data-type="friend" data-env="REAL"   data-lineId="@835gurzh" style="display: none;"></div>
+      <script src="https://www.line-website.com/social-plugins/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
+    </li>
+    <!-- Twitter -->
+    <li>
+      <p><strong>Twitter</strong></p>
+      <p>TwitterのDMでも相談受付中！！</p>
+      <a href="https://twitter.com/fukuokapzm?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @fukuokapzm</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </li>
+  </ul>
   <footer><p>Copyright 2022 福岡のパーティー絶対盛り上げる.com</p></footer>
   <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
   <script src="js/5-1-5.js"></script>

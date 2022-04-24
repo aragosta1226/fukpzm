f35ff5704093773f96f4e69a5c6aaf8d1f8d4319
aragosta1226 <djaragosta1226@gmail.com>
2022-04-24 14:20:00 +0900

    コードの追加

--- index.php
+++ index.php
@@ -1,22 +1,176 @@
+<?php
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style_login.css">
-    <title>ログイン画面</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="「福岡のパーティーを絶対に盛り上げる.com」
+  福岡でパーティーを行う際に弊社をご利用いただければ必ず盛り上げます。
+  結婚式の２次会、懇親会、文化祭、企業のパーティーなど、どこでも出張可能です。
+  会場準備や音響機材・スピーカーのご用意、イベント企画までなんでもご相談ください。">
+  <meta name="keywords" content="fukuoka,出張dj,dj派遣,福岡,fukuokaparty,
+  wedding,weddinparty,結婚式２次会,企業パーティー,ビーチパーティー,文化祭,モーターショー,
+  レセプションパーティー,ホームパーティー,クローズパーティー,キャンプパーティー,partyplanner,
+  party,sounddesign,eventplanner,福岡イベント,福岡パーティー,音響レンタル,懇親会,幹事代行,
+  beachparty,homeparty,fashionshow,fukuokacity,福岡のパーティー絶対盛り上げるドットコム">
+  <link rel="stylesheet" href="./css/style_pzm.css">
+  <title>福岡のパーティー絶対盛り上げる.com</title>
 </head>
 <body>
-    <form name="login_form">
-        <div class="login_form_top">
-            <img src="./img/pzmlogo.png" alt="ロゴ">
-        </div>
-        <div class="login_form_btn">
-            <input type="id" name="user_id" placeholder="UserID" required="required"><br>
-            <input type="password" name="password" placeholder="Password" required="required">
-            <input type="submit" name="botton" value="LOGIN">
-        </div>
-    </form>
+  <!---------------------------------------------------------------------------
+    * メインビジュアル
+  ------------------------------------------------------------------------------>
+  <div class="mainvisual">
+    <img src="./img/main.png" width="100%" height="100%" alt="福岡のパーティー絶対盛り上げるDJ機材">
+    <h1 class="main_title">DoKoDeMo DJ</h1>
+    <div class="text1">福岡のパーティー絶対盛り上げる.com</div>
+  </div> 
+  <!-----------------------------------------------------------------------------
+    * NEWS
+  ------------------------------------------------------------------------------>
+  <section class="about" id="about"> 
+      <div class="inner">
+          <h2>簡単にDJを呼べます</h2>
+          <div class="about-box">
+              <div class="about-card-wrapper">
+                  <div class="about-card">
+                      <img src="./img/dj.png" alt="DJ">
+                      <p class="about1"><strong>どんなパーティーに<br>DJを呼べるの？</strong></p>
+                      <p class="about-coment">スペースさえあればどこでもDJがあなたのパーティーに出張い<br>たします。現役のDJがパーティーの規模にあったプランをご<br>提案します。どんなパーティーにDJを呼べるのかな？等、お<br>気軽にご相談ください。ホームパーティーももちろん可！</p>
+                  </div>
+                  <div class="about-card">
+                      <img src="./img/suguyoyaku.png" alt="予約">
+                      <p class="about1"><strong>スグに予約OK</strong></p>
+                      <p class="about-coment">急にパーティーすることになった、来週DJ頼んでみようか<br>な？等でも、DJのスケジュールが空いていれば、いつでも対応<br>可能です。まずはご相談ください。</p>
+                  </div>
+                  <div class="news-card">
+                      <img src="./img/itsudemo.png" alt="確認">
+                      <p class="about1"><strong>準備の状況確認が<br>いつでも出来る！</strong></p>
+                      <p class="about-coment">これまでいちいち電話やメールで把握しないといけなかった<br>準備の状況がいつでも簡単に確認できます。譲許確認が出来る事で<br>今まで面倒だった幹事の手間を減らします。</p>
+                  </div>
+              </div>
+              <a href="#" class="news-btn">お問い合わせはこちら</a>
+          </div>
+      </div>
+  </section>
+  <h2><strong>サービスについて</strong></h2>
+  <div class="flex-item">
+    <div class="flex-item__img"><img src="./img/dj2.png" alt=""></div>
+    <div class="flex-item__txt">
+      <h2><strong>お客様のパーティーのニーズにあったDJを手配します。</strong></h2>
+      <p><strong>現役の実績のあるプロのDJが対応します！</strong></p>
+      <p>どんなシーンでも対応力のあるDJをご用意いたします。在籍しているDJは、さまざ<br>
+      まなシーンでプレイをしてきた実績のある現役のプロのDJです。もちろん事前に当<br>
+      日流して欲しい曲をリクエストすることも可能です。どんなパーティーにしたいの<br>
+      か、お客様のイメージをきちんとヒアリングいたしますのでご安心ください。</p>
+    </div>
+  </div>
+  <h2><strong>当社が選ばれる理由</strong></h2>
+  <div>
+    <div>
+      <p>point 1</p>
+      <h2>パーティーに詳しい現役DJが担当</h2>
+      <p>さまざまなシーンで実績を積んできたプロのDJがパーティーでDJするので安心して
+        開催する事ができます。どんなパーティーにしたいかをしっかりとお打ち合わせ頂きまして、
+        お客様の為に熟練のDJがイチから選曲リストを作成し、当日皆様がハッピーになっていただける様にします。</p>
+    </div>
+    <div>
+      <p>point 2</p>
+      <h2>今まで目に見えなかった進捗状況の確認ができる</h2>
+      <p>今まで準備の進み具合が確認できなくて不安だったと思います。弊社はそんな進捗状況をちゃんと確認
+        する事が出来ます。パーティー当日までの、「今どんな状況になっているのかな？」という不安を解消します。
+        主催される方の気持ちに寄り添います。</p>
+    </div>
+  </div>
+  <a href="#" class="news-btn">お問い合わせはこちら</a>
+  <h2><strong>ご相談の流れ</strong></h2>
+  <div>
+    <div>
+      <p>Step 1</p>
+      <h2>お問い合わせ</h2>
+      <p>実際にどんな事が出来るのか知りたいと思います。
+        不安な事など、一緒に解決していければと考えています。
+        まずはお気軽にお問い合わせください。</p>
+    </div>
+    <div>
+      <p>Step 2</p>
+      <h2>ヒアリング</h2>
+      <p>お問い合わせ頂きましたら、弊社よりメールにて
+        日程を決めさせて頂き、担当者のみカメラオンにてZOOM等で
+        お打ち合わせさせて頂きます。相談料などは頂きませんので、
+        お気軽にお話ししましょう！。</p>
+    </div>
+    <div>
+      <p>Step 3</p>
+      <h2>準備の進捗状況の確認</h2>
+      <p>パーティーの内容や金額に関して、気に入ってい頂きましたら、
+        再度内容の確認等を行っていただければさっそく準備に入ります。
+        準備の進捗状況に関して、丁寧に対応させて頂きます。</p>
+    </div>
+    <div>
+      <p>Step 4</p>
+      <h2>イベント当日</h2>
+      <p>イベント当日は、現場の確認等させて頂きますのでパーティー開始の
+        1時間程度前から現場入りさせて頂きますのでご安心ください。</p>
+    </div>
+  </div>
+  <a href="#" class="news-btn">お問い合わせはこちら</a>
+  <h2><strong>よくあるご質問</strong></h2>
+  <div>
+    <div>
+      <h2><strong>イベントの知識が全くないのですが、大丈夫ですか？</strong></h2>
+      <p>イベント開催について丁寧にご説明いたしますので、ご安心ください。
+        オプションで会場準備からお手伝いすることも可能です。不安なことは一緒に解決して行きましょう！！</p>
+    </div>
+    <div>
+      <h2><strong>電源がないところで開催したいのですが、可能ですか？</strong></h2>
+      <p>もちろん可能です！金額については、まずはお見積もりからさせて頂きますのでご納得頂いた上での
+        準備になります。弊社は野外イベントのノウハウなどもございますのでご安心ください。</p>
+    </div>
+  </div>
+  <!---------------------------------------------------------------------------
+      * ユーザー登録フォーム
+  ------------------------------------------------------------------------------>
+  <h2><strong>まずはお気軽にお問い合わせください！</strong></h2>
+  <form action="" method="POST">
+    <p>
+      <label for="">セイ：</label>
+      <input class="mado" type="text" name="name" required="required">
+    </p>
+    <p>
+      <label for="">メイ：</label>
+      <input class="mado" type="text" name="name" required="required">
+    </p>
+    <p>
+      <label for="email">メールアドレス：</label>
+      <input class="mado" type="email" name="email" required="required">
+    </p>
+    <p>
+    <label for="genre">お問い合わせ種別：</label>
+    <select class="genre" name="genre" required>
+      <option value="HIPHOP/R&B">選択してください。</option>
+      <option value="REGGAE">サービスについてのお問い合わせ</option>
+      <option value="EDM">機材レンタルに関してのお問い合わせ</option>
+      <option value="ANISON">その他</option>
+    </select>
+    </p>
+    <p>
+    <label for="genre">お問い合わせ内容：</label>
+    <textarea name="" id="" cols="30" rows="10"></textarea>
+    </p>
+    <p>
+      <input class="btn" type="submit" value="送信">
+    </p>
+  </form>
+  <!---------------------------------------------------------------------------
+      * ユーザー登録フォーム
+  ------------------------------------------------------------------------------>
+  <h2><strong>SNSでも相談受付してます！！</strong></h2>
+  <a href="//timeline.line.me/social-plugin/share?url=シェアするページのURL&text=" target="_blank" rel="nofollow noopener noreferrer">
+    LINE</a>
+  <footer><p>Copyright 2022 福岡のパーティー絶対盛り上げる.com</p></footer>
 </body>
 </html>
